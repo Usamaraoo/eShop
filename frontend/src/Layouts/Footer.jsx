e6090cbfa888788a1ca87c465278a74535960f62
Bilal478 <478bilal@gmail.com>
2023-02-07 12:19:54 +0500

    footer responsive

--- frontend/src/Layouts/Footer.jsx
+++ frontend/src/Layouts/Footer.jsx
@@ -5,10 +5,10 @@ export default function Footer() {
     return (
         <footer className=' p-4 bg-black text-white'>
             <div className='flex justify-between'>
-                <Link to='/' className='tracking-widest text-2xl text-bold'>
+                <Link to='/' className='tracking-widest md:text-2xl text-xl text-bold'>
                     eShop
                 </Link>
-                <div className='flex gap-2 text-sm items-center'>
+                <div className='flex gap-2 md:text-sm items-center text-xs'>
                     <p>Home</p>
                     <p>Products</p>
                     <p>Careers</p>
